11e3e6b5f86d302454cebc2fd9b815c3b7dcfd72
Jackson Warner <jackson_war14@icloud.com>
2024-12-10 14:01:57 -0800

    Create BLEFunctions.cpp

---
+++ SonicIoT/BLEFunctions.cpp
@@ -0,0 +1,17 @@
+#include "BLEFunctions.h"
+
+void badBLE() {
+  // Implement Bad BLE function
+}
+
+void iosSpam() {
+  // Implement IOS Spam function
+}
+
+void androidSpam() {
+  // Implement Android Spam function
+}
+
+void windowsSpam() {
+  // Implement Windows Spam function
+}
