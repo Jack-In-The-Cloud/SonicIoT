9c6438c21332d801b66f57e3ea168cdf958b5dc5
Jackson Warner <jackson_war14@icloud.com>
2024-12-10 14:04:01 -0800

    Create RFIDFunctions.cpp

---
+++ SonicIoT/RFIDFunctions.cpp
@@ -0,0 +1,25 @@
+#include "RFIDFunctions.h"
+
+void readTag() {
+  // Implement Read Tag function
+}
+
+void cloneTag() {
+  // Implement Clone Tag function
+}
+
+void writeNDEF() {
+  // Implement Write NDEF Records function
+}
+
+void writeData() {
+  // Implement Write Data function
+}
+
+void eraseData() {
+  // Implement Erase Data function
+}
+
+void saveLoadFiles() {
+  // Implement Save/Load Files function
+}
